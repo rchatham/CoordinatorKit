ae7d7d59df9345624c1a99f2934527f63654ac5d
Nathan Lanza <nathan@lanza.io>
2017-01-18 01:52:26 -0500

    Change UIViewController's getter for  to typecast for  since UIKit uses some hidden s that won't have a coordinator.

--- Source/UIViewController+CoordinatorKit.swift
+++ Source/UIViewController+CoordinatorKit.swift
@@ -1,68 +1,76 @@
-import UIKit
-
-//MARK: - Swizzling
-fileprivate let swizzling: (UIViewController.Type) -> () = { viewController in
-    
-    let swizzle: (Selector,Selector) -> () = { first, second in
-        let originalMethod = class_getInstanceMethod(viewController, first)
-        let swizzledMethod = class_getInstanceMethod(viewController, second)
+        import UIKit
         
-        method_exchangeImplementations(originalMethod, swizzledMethod)
-    }
-    
-    let vwa = #selector(viewController.viewWillAppear(_:))
-    let vwa2 = #selector(viewController.ck_viewWillAppear(_:))
-   
-    let vda = #selector(viewController.viewDidAppear(_:))
-    let vda2 = #selector(viewController.ck_viewDidAppear(_:))
-   
-    let vwd = #selector(viewController.viewWillDisappear(_:))
-    let vwd2 = #selector(viewController.ck_viewWillDisappear(_:))
-    
-    let vdd = #selector(viewController.viewDidDisappear(_:))
-    let vdd2 = #selector(viewController.ck_viewDidDisappear(_:))
-    
-    for (l,r) in [(vwa,vwa2),(vda,vda2),(vwd,vwd2),(vdd,vdd2)] {
-        swizzle(l,r)
-    }
-}
-
-extension UIViewController {
-    
-    func ck_viewWillAppear(_ animated: Bool) {
-        coordinator.willNavigateToViewController(animated)
-        ck_viewWillAppear(animated)
-    }
-    func ck_viewDidAppear(_ animated: Bool) {
-        coordinator.didNavigateToViewController(animated)
-        ck_viewDidAppear(animated)
-    }
-    func ck_viewWillDisappear(_ animated: Bool) {
-        coordinator.willNavigateAwayFromViewController(animated)
-        ck_viewWillDisappear(animated)
-    }
-    func ck_viewDidDisappear(_ animated: Bool) {
-        coordinator.didNavigateAwayFromViewController(animated)
-        ck_viewDidDisappear(animated)
-    }
-    
-    open override class func initialize() {
-        guard self === UIViewController.self else { return }
-        swizzling(self)
-    }
-}
-
-//MARK: - Associated Objects
-extension UIViewController {
-    private struct AssociatedKeys {
-        static var coordinator = "ck_coordinator"
-    }
-    public var coordinator: Coordinator! {
-        get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.coordinator) as! Coordinator
+        //MARK: - Swizzling
+        fileprivate let swizzling: (UIViewController.Type) -> () = { viewController in
+            
+            let swizzle: (Selector,Selector) -> () = { first, second in
+                let originalMethod = class_getInstanceMethod(viewController, first)
+                let swizzledMethod = class_getInstanceMethod(viewController, second)
+                
+                method_exchangeImplementations(originalMethod, swizzledMethod)
+            }
+            
+            let vwa = #selector(viewController.viewWillAppear(_:))
+            let vwa2 = #selector(viewController.ck_viewWillAppear(_:))
+            
+            let vda = #selector(viewController.viewDidAppear(_:))
+            let vda2 = #selector(viewController.ck_viewDidAppear(_:))
+            
+            let vwd = #selector(viewController.viewWillDisappear(_:))
+            let vwd2 = #selector(viewController.ck_viewWillDisappear(_:))
+            
+            let vdd = #selector(viewController.viewDidDisappear(_:))
+            let vdd2 = #selector(viewController.ck_viewDidDisappear(_:))
+            
+            for (l,r) in [(vwa,vwa2),(vda,vda2),(vwd,vwd2),(vdd,vdd2)] {
+                swizzle(l,r)
+            }
         }
-        set {
-            objc_setAssociatedObject(self, &AssociatedKeys.coordinator, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        
+        extension UIViewController {
+            
+            func ck_viewWillAppear(_ animated: Bool) {
+                if let coordinator = coordinator {
+                    coordinator.willNavigateToViewController(animated)
+                }
+                ck_viewWillAppear(animated)
+            }
+            func ck_viewDidAppear(_ animated: Bool) {
+                if let coordinator = coordinator {
+                    coordinator.didNavigateToViewController(animated)
+                }
+                ck_viewDidAppear(animated)
+            }
+            func ck_viewWillDisappear(_ animated: Bool) {
+                if let coordinator = coordinator {
+                    coordinator.willNavigateAwayFromViewController(animated)
+                }
+                ck_viewWillDisappear(animated)
+            }
+            func ck_viewDidDisappear(_ animated: Bool) {
+                if let coordinator = coordinator {
+                    coordinator.didNavigateAwayFromViewController(animated)
+                }
+                ck_viewDidDisappear(animated)
+            }
+            
+            open override class func initialize() {
+                guard self === UIViewController.self else { return }
+                swizzling(self)
+            }
         }
-    }
-}
+        
+        //MARK: - Associated Objects
+        extension UIViewController {
+            private struct AssociatedKeys {
+                static var coordinator = "ck_coordinator"
+            }
+            public var coordinator: Coordinator! {
+                get {
+                    return objc_getAssociatedObject(self, &AssociatedKeys.coordinator) as? Coordinator
+                }
+                set {
+                    objc_setAssociatedObject(self, &AssociatedKeys.coordinator, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                }
+            }
+        }
